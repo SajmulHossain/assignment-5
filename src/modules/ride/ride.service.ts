326eb45f6e27a04bb4181d54b0d31db0bef23e80
Sajmul <sajmul1427@gmail.com>
2025-07-30 21:12:18 +0600

    if a rider already in a ride, then he cannot request for another ride

--- src/modules/ride/ride.service.ts
+++ src/modules/ride/ride.service.ts
@@ -6,6 +6,14 @@ import { IRide, RideStatus } from "./ride.interface";
 import { Ride } from "./ride.model";
 
 const createRide = async (payload: IRide, user: IUser) => {
+  const { email } = user;
+
+  const isRunningRide = await Ride.findOne({rider: email});
+
+  if(isRunningRide) {
+    throw new AppError(400, "You are already in a ride");
+  }
+
   const coordinate1 = payload.pickup;
   const coordinate2 = payload.destination;
 
@@ -17,54 +25,63 @@ const createRide = async (payload: IRide, user: IUser) => {
   const amount = Math.round(distance * 0.025);
 
   payload.amount = amount;
-  payload.rider = user.email;
+  payload.rider = email;
 
   const ride = await Ride.create(payload);
 
   return ride;
 };
 
-const updateRideStatus = async (id: string, payload: Record<string,string>, user: IUser) => {
+const updateRideStatus = async (
+  id: string,
+  payload: Record<string, string>,
+  user: IUser
+) => {
   const { status } = payload as { status: string };
   const { email } = user;
 
-  const isAvailableDriver = await User.findOne({ email, role: UserRole.driver, isDriverActive: true, driverApprovalStatus: DriverApprovalStatus.approve })
+  const isAvailableDriver = await User.findOne({
+    email,
+    role: UserRole.driver,
+    isDriverActive: true,
+    driverApprovalStatus: DriverApprovalStatus.approve,
+  });
 
-  if(!isAvailableDriver) {
+  if (!isAvailableDriver) {
     throw new AppError(404, "Driver is not available");
   }
 
-  if(!isAvailableDriver?.vehicleInfo?.model) {
+  if (!isAvailableDriver?.vehicleInfo?.model) {
     throw new AppError(400, "Please update your vehicle info");
   }
 
-  
   const ride = await Ride.findById(id);
-  
-  if(ride?.rider === isAvailableDriver.email) {
+
+  if (ride?.rider === isAvailableDriver.email) {
     throw new AppError(400, "You cannot handle your requested ride");
   }
 
   const isOnRide = await Ride.findOne({ driver: email });
 
-  console.log(isOnRide);
-
-  if(isOnRide) {
-    throw new AppError(400, "You cannot accept another ride during a in transiter ride");
+  if (isOnRide) {
+    throw new AppError(
+      400,
+      "You cannot accept another ride during a in transiter ride"
+    );
   }
 
-  if(!ride){
+  if (!ride) {
     throw new AppError(404, "Ride not found");
   }
 
-  if(status === RideStatus.requested) {
+  if (status === RideStatus.requested) {
     throw new AppError(400, "Invalid request");
   }
 
-  if(user.role === UserRole.rider && status !== RideStatus.cancelled) {
+  if (user.role === UserRole.rider && status !== RideStatus.cancelled) {
     throw new AppError(400, "Only driver can do this");
   }
-  
+
   if (
     status === RideStatus.cancelled &&
     ride?.status.some((singleRide) => singleRide.state !== RideStatus.requested)
@@ -75,26 +92,36 @@ const updateRideStatus = async (id: string, payload: Record<string,string>, user
     );
   }
 
-  if (ride?.status.some((singleRide) => singleRide.state === status)
-  ) {
+  if (ride?.status.some((singleRide) => singleRide.state === status)) {
     throw new AppError(400, `Ride is already ${status}`);
   }
-  
+
   const rideStatusArr = Object.values(RideStatus);
 
-  const currentStatus = ride.status[ride.status.length-1].state;
-  
-  const rideCurrentIndex = rideStatusArr.findIndex(rideValue => rideValue === currentStatus);
+  const currentStatus = ride.status[ride.status.length - 1].state;
 
-  const rideUpdateStatusIndex = rideStatusArr.findIndex(ride => ride === status);
+  const rideCurrentIndex = rideStatusArr.findIndex(
+    (rideValue) => rideValue === currentStatus
+  );
 
-  if(rideUpdateStatusIndex !== rideCurrentIndex + 1) {
-    throw new AppError(400, `Give sequencial update. e.g, ${Object.values(RideStatus)}`);
+  const rideUpdateStatusIndex = rideStatusArr.findIndex(
+    (ride) => ride === status
+  );
+
+  if (rideUpdateStatusIndex !== rideCurrentIndex + 1) {
+    throw new AppError(
+      400,
+      `Give sequencial update. e.g, ${Object.values(RideStatus)}`
+    );
   }
 
-  const updatedStatus = [...ride.status, {state: status}]
+  const updatedStatus = [...ride.status, { state: status }];
 
-  const data = await Ride.findByIdAndUpdate(id, { status: updatedStatus, driver: email }, { new: true });
+  const data = await Ride.findByIdAndUpdate(
+    id,
+    { status: updatedStatus, driver: email },
+    { new: true }
+  );
 
   return data;
 };
