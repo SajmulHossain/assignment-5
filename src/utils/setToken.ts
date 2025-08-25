8b8522a3cc1583a295a7dd63783bd079e2d812c1
Sajmul <sajmul1427@gmail.com>
2025-08-25 19:05:24 +0600

    logout problem in production solved

--- src/utils/setToken.ts
+++ src/utils/setToken.ts
@@ -6,25 +6,29 @@ interface IToken {
   refreshToken?: string;
 }
 
-export const setToken = (res:Response, token: IToken) => {
+export const setToken = (res: Response, token: IToken) => {
   if (token.accessToken) {
     res.cookie("accessToken", token.accessToken, {
-        httpOnly: true,
-        secure: env.node_env === "production",
-        sameSite: "none"
-    })
+      httpOnly: true,
+      secure: env.node_env === "production",
+      sameSite: env.node_env === "production" ? "none" : "strict",
+    });
   }
 
-  if(token.refreshToken) {
+  if (token.refreshToken) {
     res.cookie("refreshToken", token.refreshToken, {
       httpOnly: true,
       secure: env.node_env === "production",
-      sameSite: "none",
+      sameSite: env.node_env === "production" ? "none" : "strict",
     });
   }
 };
 
 export const clearCookie = (res: Response) => {
-    res.clearCookie("accessToken")
-    res.clearCookie("refreshToken")
-}
+  res.clearCookie("accessToken", {
+    httpOnly: true,
+    secure: env.node_env === "production",
+    sameSite: env.node_env === "production" ? "none" : "strict",
+  });
+  res.clearCookie("refreshToken");
+};
